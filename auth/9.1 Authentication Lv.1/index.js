ac4535bc01ea1e52f3d25a82c12e8db76a775ad1
sohamp321 <sohamp321@gmail.com>
2025-06-05 17:10:46 +0530

    Completed Hashing

--- auth/9.1 Authentication Lv.1/index.js
+++ auth/9.1 Authentication Lv.1/index.js
@@ -1,8 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "secrets",
+  password: "1093",
+  port: 5432,
+});
+
+db.connect();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -19,9 +30,57 @@ app.get("/register", (req, res) => {
   res.render("register.ejs");
 });
 
-app.post("/register", async (req, res) => {});
+app.post("/register", async (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
 
-app.post("/login", async (req, res) => {});
+  const ifExists = await db.query("SELECT * FROM users WHERE email = $1", [
+    email,
+  ]);
+
+  if (ifExists.rows.length > 0) {
+    res.status(409).redirect("/login");
+    return;
+  } else {
+    try {
+      const result = await db.query(
+        "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *",
+        [email, password]
+      );
+      console.log("User registered: ", result.rows[0]);
+      res.status(201);
+      res.render("secrets.ejs");
+      return;
+    } catch (err) {
+      console.log("Error registering user: ", err);
+      res.status(500).send("Error registering user");
+      return;
+    }
+  }
+});
+
+app.post("/login", async (req, res) => {
+  const email = req.body.username;
+  const password = req.body.password;
+  try {
+    const userExists = await db.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
+
+    if (userExists.rows.length > 0) {
+      const userPassword = userExists.rows[0].password;
+      if (userPassword === password) {
+        res.render("secrets.ejs");
+      } else {
+        res.status(401).send("Password Incorrect.");
+      }
+    } else {
+      res.redirect("/register");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
